eb91950181a22b7647f750012dc7fdf3da63738a
05555 <05555@qq.com>
2018-05-17 21:31:33 +0800

    auto click to login page

--- amazon/main.js
+++ amazon/main.js
@@ -5,16 +5,18 @@ $(function(){
 	var cookie_sess_at_main = $.cookie("sess-at-main");
 	
 	//already logined
-	if (cookie_x_main != null && cookie_at_main != null && cookie_sess_at_main != null){
+	if (cookie_x_main != null || cookie_at_main != null || cookie_sess_at_main != null){
 		//ok
+		window.location.href = "https://www.amazon.com/s/&field-keywords=toilet light &field-brandtextbin=Proslth";
 	}else {
 		//unlogin
 		if (window.location.href.indexOf("signin") > 0){
 			autoSign();
 		}else{
-			//if ($("a[class='nav-action-button']").length == 0)
-			$("#nav-link-accountList").click()
-			$("a[class='nav-action-button']").click();
+			if ($("#nav-link-accountList").length && $("#nav-link-accountList").length > 0)
+			     $("#nav-link-accountList")[0].click();
+			if ($("a[class='nav-action-button']"))
+			     $("a[class='nav-action-button']").click();
 		}
 	}
 	
